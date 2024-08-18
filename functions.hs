fd804d950665f48474c3c571fb6743651063a9e7
Rodrigo Sandler <141432856+SandlerRodrigo@users.noreply.github.com>
2024-08-18 22:44:06 +0000

    added function to bee1018 problem - banknotes

--- functions.hs
+++ functions.hs
@@ -1,3 +1,10 @@
 fib x
     | x < 2 = 1
     | otherwise = fib (x - 1) + fib (x - 2)
+
+cedulas :: Int -> [Int] -> [Int] -> [Int]
+cedulas 0 _ resultado = resultado
+cedulas valor (nota:notas) resultado =
+    let contador = valor `div` nota
+        resto = valor `mod` nota
+    in cedulas resto notas (resultado ++ [contador])
